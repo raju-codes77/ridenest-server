9ab7f4287ec423551a771bb809bd6ac2bfec47b1
raju2277 <yajuddinraju1@gmail.com>
2026-05-19 04:13:46 -0700

    server created

---
+++ index.js
@@ -0,0 +1,54 @@
+const express=require('express')
+const dotenv=require('dotenv')
+const cors=require ('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config()
+
+const uri = process.env.MONGODB_URI;
+const app=express()
+
+const PORT=process.env.PORT
+app.use(cors())
+app.use(express.json())
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db=client.db("ridenest")
+    const carCollection=db.collection("car")
+
+    app.post('/car',async(req,res)=>{
+        const carData=req.body
+       const result=await carCollection.insertOne(carData)
+
+       res.json(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send("ridenest server is running fine")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
